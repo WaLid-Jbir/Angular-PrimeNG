398b320ae3743396576d622365c5ec08dc055fb4
WaLid-Jbir <anawalid737@gmail.com>
2025-05-15 15:50:52 +0100

    Creating login form card with primeNG

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { Message } from 'primeng/message';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CardModule, FormsModule, InputTextModule, Message, PasswordModule, ButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  login = {
+    email: '',
+    password: ''
+  }
+
+  onLogin(){
+    console.log(this.login);
+  }
 
 }
