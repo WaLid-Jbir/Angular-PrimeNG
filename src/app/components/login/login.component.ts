91ef2d146782adb79610f7a0fd7a3064ba457522
WaLid-Jbir <anawalid737@gmail.com>
2025-05-18 03:34:11 +0100

    Adding validation to register form fields & making sure that the password and confirm password validation are working fine

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
@@ -8,7 +9,7 @@ import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-login',
-  imports: [CardModule, FormsModule, InputTextModule, Message, PasswordModule, ButtonModule],
+  imports: [CardModule, FormsModule, InputTextModule, Message, PasswordModule, ButtonModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
